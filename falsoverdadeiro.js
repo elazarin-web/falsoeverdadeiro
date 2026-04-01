6c8138607285656af862f5bb0c23a2107885bb84
elazarin-web <e.lazarin@escola.pr.gov.br>
2026-04-01 15:20:07 +0000

    conversoes

--- falsoverdadeiro.js
+++ falsoverdadeiro.js
@@ -1,7 +1,21 @@
-const usuariologado = true;
-const contapaga = false;
+//const usuariologado = true;
+//const contapaga = false;
 
 // 0 ==> false
 // 1  ==> true
 
-console.log (1 == true) 
+//console.log (0 == false);
+//console.log (1 == true);
+//console.log ("" == false);
+
+// null
+// undefined
+
+let minhaVar;
+let varNull = null;
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(typeof minhaVar)
+console.log(typeof varNull)
