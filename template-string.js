c8e3109c74abdfe8b1f11e157e86a1992d23fa3f
elazarin-web <e.lazarin@escola.pr.gov.br>
2026-04-28 15:23:58 +0000

    template

---
+++ template-string.js
@@ -0,0 +1,8 @@
+const nome = "kauã";
+const idade = 2026-2009;
+const cidadeDeNascimento ="Roncador"
+
+//const apresentação = " Meu namorado é o " + nome + ", sua idade é " + idade + " e nasceu na cidade de " + cidadeDeNascimento;
+
+const aprentacao = 'Meu namorado '
+console.log(apresentação);
