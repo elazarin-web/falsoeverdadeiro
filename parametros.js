c7486fccd58fad87e0db9a1756c26a37b312329d
elazarin-web <e.lazarin@escola.pr.gov.br>
2026-05-04 15:25:16 +0000

    4

--- parametros.js
+++ parametros.js
@@ -1,12 +1,22 @@
+//function soma(numero1, numero2){
+  //  return numero1 + numero2;
+//}
+//console.log(soma(2,2));
+//console.log(soma(245,20));
+//console.log(soma(-500,100));
+
+//function nomeIdade(nome, idade){
+  //  return `Meu nome é ${nome} e minha idade é ${idade}`;
+//}
+
+//console.log(nomeIdade("Eduarda", 17));
+
 function soma(numero1, numero2){
     return numero1 + numero2;
 }
-console.log(soma(2,2));
-console.log(soma(245,20));
-console.log(soma(-500,100));
 
-function nomeIdade(nome, idade){
-    return `Meu nome é ${nome} e minha idade é ${idade}`;
+function multiplica (numero1, numero2){
+    return numero1 * numero2;
 }
 
-console.log(nomeIdade("Eduarda", 17));
+    console.log(multiplica(soma(4,5), soma(3,3)));
